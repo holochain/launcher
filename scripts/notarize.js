80991a8a5ea56b0eabbbdde75f47d00c04249d4d
Matthias <tuttiacc@hotmail.com>
2024-01-08 17:17:14 +0100

    added code-signing and notarizing

---
+++ scripts/notarize.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+const fs = require('fs');
+const path = require('path');
+const { notarize } = require('@electron/notarize');
+
+module.exports = async function (params) {
+  if (process.platform !== 'darwin') {
+    return;
+  }
+
+  console.log('afterSign hook triggered', params);
+
+  const appId = 'holochain.launcher.electron.prototype';
+
+  const appPath = path.join(params.appOutDir, `${params.packager.appInfo.productFilename}.app`);
+  if (!fs.existsSync(appPath)) {
+    console.log('skip');
+    return;
+  }
+
+  console.log(`Notarizing ${appId} found at ${appPath}`);
+
+  await notarize({
+    appBundleId: appId,
+    appPath: appPath,
+    appleId: process.env.APPLE_ID_EMAIL,
+    appleIdPassword: process.env.APPLE_ID_PASSWORD,
+    teamId: process.env.APPLE_TEAM_ID,
+  });
+  console.log(`Done notarizing ${appId}`);
+};
