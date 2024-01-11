4f1eaee1dc696d166827062791075faca533f37e
Dawid Urbas <dawid@getbold.agency>
2024-01-11 13:46:49 +0000

    Remove console.log

--- src/renderer/src/lib/helpers/holochain.ts
+++ src/renderer/src/lib/helpers/holochain.ts
@@ -1,12 +1,7 @@
 import { type CellId, CellInfoSchema } from '../../../../types';
 
 export function getCellId(cellInfo: unknown): CellId | undefined {
-	console.log('getCellId', cellInfo);
 	const parsedCellInfo = CellInfoSchema.safeParse(cellInfo);
-
-	if (!parsedCellInfo.success) {
-		console.log(parsedCellInfo.error);
-	}
 
 	if (!parsedCellInfo.success || 'stem' in parsedCellInfo.data) {
 		return undefined;
