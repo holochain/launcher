f02c951ebe6a66217ee599cc399580d9953d673b
Dawid Urbas <dawid@getbold.agency>
2024-01-11 10:02:02 +0000

    Fix format

--- src/renderer/src/routes/splash/+layout.svelte
+++ src/renderer/src/routes/splash/+layout.svelte
@@ -1,5 +1,5 @@
-<div class="bg-login-background relative flex h-screen flex-col bg-fixed bg-top bg-no-repeat">
-	<div class="bg-tertiary-900 absolute inset-0 opacity-50" />
+<div class="relative flex h-screen flex-col bg-login-background bg-fixed bg-top bg-no-repeat">
+	<div class="absolute inset-0 bg-tertiary-900 opacity-50" />
 	<!-- White overlay div -->
 	<p class="z-10 p-1 text-center text-xs opacity-30">Holochain Beta 0.1</p>
 	<div class="max-w-s z-10 flex flex-1 flex-col items-center justify-center text-center">
