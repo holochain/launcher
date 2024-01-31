b01607934c4d8d412f52f4843390c9b5cf31e1ef
Matthias <tuttiacc@hotmail.com>
2024-01-31 16:06:03 +0100

    bumped to holochain from PR

--- src/main/binaries.ts
+++ src/main/binaries.ts
@@ -1,14 +1,14 @@
 import { app } from 'electron';
 import * as path from 'path';
 
-export const DEFAULT_HOLOCHAIN_VERSION = '0.2.5-rc.1';
+export const DEFAULT_HOLOCHAIN_VERSION = '0.2.5-rc.1-8613839';
 
 const BINARIES_DIRECTORY = app.isPackaged
   ? path.join(app.getAppPath(), '../app.asar.unpacked/resources/bins')
   : path.join(app.getAppPath(), './resources/bins');
 
 const HOLOCHAIN_BINARIES = {
-  '0.2.5-rc.1': path.join(
+  '0.2.5-rc.1-8613839': path.join(
     BINARIES_DIRECTORY,
     `holochain-v${DEFAULT_HOLOCHAIN_VERSION}${process.platform === 'win32' ? '.exe' : ''}`,
   ),
