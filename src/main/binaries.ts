3b56f81a340d80a6749dc66fa9ef663199150595
Dawid Urbas <dawid@getbold.agency>
2024-02-01 12:02:39 +0000

    Test flow

--- src/main/binaries.ts
+++ src/main/binaries.ts
@@ -1,15 +1,30 @@
 import { app } from 'electron';
 import fs from 'fs';
+import { readFileSync } from 'fs';
 import * as path from 'path';
 
-export const DEFAULT_HOLOCHAIN_VERSION = '0.2.5-rc.1-8613839';
+import { HolochainLairVersionSchema } from '../types';
 
-const BINARIES_DIRECTORY = app.isPackaged
-  ? path.join(app.getAppPath(), '../app.asar.unpacked/resources/bins')
-  : path.join(app.getAppPath(), './resources/bins');
+const getFilePath = (relativePath) => path.join(app.getAppPath(), relativePath);
+
+const getPackageJSONPath = () =>
+  getFilePath(app.isPackaged ? '../app.asar.unpacked/package.json' : './package.json');
+
+const readJSONFile = (filePath) => JSON.parse(readFileSync(filePath, 'utf8'));
+
+const packageJSON = readJSONFile(getPackageJSONPath());
+
+const HolochainLairVersion = HolochainLairVersionSchema.parse(packageJSON);
+
+export const DEFAULT_HOLOCHAIN_VERSION = HolochainLairVersion.binaries.holochain;
+export const DEFAULT_LAIR_KEYSTORE_VERSION = HolochainLairVersion.binaries.lair_keystore;
+
+const BINARIES_DIRECTORY = getFilePath(
+  app.isPackaged ? '../app.asar.unpacked/resources/bins' : './resources/bins',
+);
 
 const HOLOCHAIN_BINARIES = {
-  '0.2.5-rc.1-8613839': path.join(
+  DEFAULT_HOLOCHAIN_VERSION: path.join(
     BINARIES_DIRECTORY,
     `holochain-v${DEFAULT_HOLOCHAIN_VERSION}${process.platform === 'win32' ? '.exe' : ''}`,
   ),
@@ -17,7 +32,7 @@ const HOLOCHAIN_BINARIES = {
 
 const LAIR_BINARY = path.join(
   BINARIES_DIRECTORY,
-  `lair-keystore-v0.4.2${process.platform === 'win32' ? '.exe' : ''}`,
+  `lair-keystore-v${DEFAULT_LAIR_KEYSTORE_VERSION}${process.platform === 'win32' ? '.exe' : ''}`,
 );
 
 export const checkHolochainLairBinariesExist = () =>
