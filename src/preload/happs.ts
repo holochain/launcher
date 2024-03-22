b7218f2246ceb5a1e94329a5656e91710d3c9700
Matthias <tuttiacc@hotmail.com>
2024-03-22 09:00:38 +0100

    expose zome call signing to admin window, add new zome call signing method to happ windows

--- src/preload/happs.ts
+++ src/preload/happs.ts
@@ -1,8 +1,14 @@
+import type { CallZomeRequestUnsigned } from '@holochain/client';
 import { contextBridge, ipcRenderer } from 'electron';
 import type { ZomeCallUnsignedNapi } from 'hc-launcher-rust-utils';
 
+contextBridge.exposeInMainWorld('__HC_ZOME_CALL_SIGNER__', {
+  signZomeCall: (request: CallZomeRequestUnsigned) => ipcRenderer.invoke('sign-zome-call', request),
+});
+
 const contextBridgeApi = {
-  signZomeCall: (zomeCall: ZomeCallUnsignedNapi) => ipcRenderer.invoke('sign-zome-call', zomeCall),
+  signZomeCall: (zomeCall: ZomeCallUnsignedNapi) =>
+    ipcRenderer.invoke('sign-zome-call-legacy', zomeCall),
 };
 
 export type ContextBridgeApi = typeof contextBridgeApi;
