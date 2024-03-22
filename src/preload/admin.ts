aa8a83e3c2f4629163348d34fed73b0773b053fb
Matthias <tuttiacc@hotmail.com>
2024-03-22 09:00:38 +0100

    expose zome call signing to admin window, add new zome call signing method to happ windows

--- src/preload/admin.ts
+++ src/preload/admin.ts
@@ -1,4 +1,10 @@
+import type { CallZomeRequestUnsigned } from '@holochain/client';
+import { contextBridge, ipcRenderer } from 'electron';
 import { exposeElectronTRPC } from 'electron-trpc/main';
+
+contextBridge.exposeInMainWorld('__HC_ZOME_CALL_SIGNER__', {
+  signZomeCall: (request: CallZomeRequestUnsigned) => ipcRenderer.invoke('sign-zome-call', request),
+});
 
 process.once('loaded', async () => {
   exposeElectronTRPC();
