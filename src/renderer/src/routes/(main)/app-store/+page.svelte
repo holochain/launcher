f1746adc555a389b9c4120aad331afb3e398c508
Dawid Urbas <dawid@getbold.agency>
2024-04-16 11:52:12 +0200

    Fix structure

--- src/renderer/src/routes/(main)/app-store/+page.svelte
+++ src/renderer/src/routes/(main)/app-store/+page.svelte
@@ -1,8 +1,4 @@
 <script lang="ts">
-	import { getModalStore } from '@skeletonlabs/skeleton';
-
-	import { MODAL_INSTALL_KANDO } from '$const';
-	import { createModalParams } from '$helpers';
 	import { createAppQueries } from '$queries';
 	import { APP_STORE } from '$shared/const';
 
@@ -25,12 +21,6 @@
 			searchInput = '';
 		}
 	}
-
-	const modalStore = getModalStore();
-
-	const modal = createModalParams(MODAL_INSTALL_KANDO);
-
-	const commonButtonClasses = 'card flex cursor-pointer items-center p-4 dark:variant-soft-warning';
 </script>
 
 <MainHeader {handlePress} bind:searchInput type={APP_STORE} />
@@ -38,14 +28,10 @@
 <div class="grow bg-light-background p-4 dark:bg-apps-list-dark-gradient">
 	<div class="text-token grid w-full gap-4 md:grid-cols-2">
 		{#each $appStoreHappsQuery.isSuccess ? $appStoreHappsQuery.data : [] as app}
-			<a class={commonButtonClasses} href={`/${APP_STORE}/${app.id}`}>
-				<AppCard icon={app.icon} title={app.title} subtitle={app.subtitle} />
-			</a>
+			<AppCard icon={app.icon} title={app.title} subtitle={app.subtitle} id={app.id} />
 		{/each}
 		{#if isKandoInSearch}
-			<button on:click={() => modalStore.trigger(modal)} class={commonButtonClasses}>
-				<AppCard />
-			</button>
+			<AppCard />
 		{/if}
 		<InstallFromDeviceCard />
 	</div>
