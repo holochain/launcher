b2937213e408ba18ac6a6bd906926b75d07e01ba
Matthias <tuttiacc@hotmail.com>
2024-06-17 11:41:02 +0200

    add whitelist types and add whitelisting logic to checkForUiUpdates method

--- libs/appstore/src/appstore/appstore-app-client.ts
+++ libs/appstore/src/appstore/appstore-app-client.ts
@@ -14,7 +14,7 @@ import type {
 } from '../devhub/types';
 import { MereMemoryZomeClient } from '../mere-memory/zomes/mere-memory-zome-client';
 import { PortalZomeClient } from '../portal/zomes/portal-zome-client';
-import type { UpdateEntityInput } from '../types';
+import type { AppStoreWhiteList, UpdateEntityInput } from '../types';
 import { bundleToDeterministicBytes } from '../utils';
 import type {
   AppEntry,
@@ -90,27 +90,29 @@ export class AppstoreAppClient {
   }
 
   /**
-   * Checks whether a new update is available for a given AppVersionEntry action hash
+   * Checks whether a new update is available for a given AppVersionEntry id
    *
    * An update is considered valid if
    * - the happ sha256 is identical to the happ sha256 of the existing AppVersionEntry
    * - the ui sha256 is different from the ui sha256 of the existing AppVersionEntry
    * - the AppVersionEntry is published later than the current AppVersionEntry
    *
-   * @param appVersionActionHash
+   * @param appVersionId
+   * @param whitelist mandatory whitelist by which UI updates are filtered
    * @returns
    */
   async checkForUiUpdate(
-    appVersionActionHash: ActionHash,
+    appVersionId: ActionHash,
+    whitelist: AppStoreWhiteList,
   ): Promise<Entity<AppVersionEntry> | undefined> {
     // logic
     // we need to check that there is a new version available and that the happ bundle hash is the same but the ui hash is different
-    const appVersionEntity = await this.appstoreZomeClient.getAppVersion(appVersionActionHash);
+    const appVersionEntity = await this.appstoreZomeClient.getAppVersion(appVersionId);
     const appVersions = await this.appstoreZomeClient.getAppVersionsForApp(
       appVersionEntity.content.for_app,
     );
     // Check for newer versions
-    const updateCandidates = appVersions
+    let updateCandidates = appVersions
       .filter((entity) => entity.content.published_at > appVersionEntity.content.published_at)
       .filter(
         (entity) =>
@@ -122,6 +124,13 @@ export class AppstoreAppClient {
           entity.content.bundle_hashes.ui_hash !== appVersionEntity.content.bundle_hashes.ui_hash,
       )
       .sort((a, b) => b.content.published_at - a.content.published_at);
+
+    const appEntryList = whitelist[encodeHashToBase64(appVersionEntity.content.for_app)];
+    if (appEntryList && appEntryList.appVersions !== 'all') {
+      updateCandidates = updateCandidates.filter((entity) =>
+        appEntryList.appVersions.includes(encodeHashToBase64(entity.action)),
+      );
+    }
 
     if (updateCandidates.length > 0) return updateCandidates[0];
     return undefined;
--- libs/appstore/src/types.ts
+++ libs/appstore/src/types.ts
@@ -1,4 +1,4 @@
-import type { ActionHash, AgentPubKey } from '@holochain/client';
+import type { ActionHash, ActionHashB64, AgentPubKey } from '@holochain/client';
 
 export type UpdateEntityInput<T> = {
   base: ActionHash;
@@ -8,4 +8,30 @@ export type UpdateEntityInput<T> = {
 export type AgentInfo = {
   agent_initial_pubkey: AgentPubKey;
   agent_latest_pubkey: AgentPubKey;
+};
+
+export type AppstoreFilterLists = {
+  /**
+   * Named whitelists
+   */
+  whitelists: Record<string, AppStoreWhiteList>;
+
+  /**
+   * A blacklist containing action hashes of AppEntrys that should be hidden completely
+   */
+  blacklist: Array<ActionHashB64>;
+};
+
+/**
+ * A whitelist with AppEntry ids as keys
+ */
+export type AppStoreWhiteList = Record<ActionHashB64, AppEntryWhitelist>;
+
+/**
+ * A whitelist of AppEntry actions and ids of AppVersion entries associated
+ * to the given AppEntry
+ */
+export type AppEntryWhitelist = {
+  actions: Array<ActionHashB64> | 'all';
+  appVersions: Array<ActionHashB64> | 'all';
 };
