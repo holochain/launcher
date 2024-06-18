1376575e51d3b57069155f72928e40aa1450084a
Dawid Urbas <dawid@getbold.agency>
2024-06-18 14:59:28 +0200

    Fix script

--- scripts/update-electron-builder.js
+++ scripts/update-electron-builder.js
@@ -9,8 +9,8 @@ const electronBuilderYmlPath = path.join(__dirname, '..', 'electron-builder.yml'
 // Read package.json
 const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'));
 
-// Extract the holochain version
-const holochainVersion = packageJson.binaries.holochain;
+// Extract the non-breaking holochain version
+const holochainVersion = packageJson.binaries.holochain.split('.').slice(0, 2).join('.');
 
 // Read electron-builder.yml
 const electronBuilderYml = yaml.load(fs.readFileSync(electronBuilderYmlPath, 'utf8'));
