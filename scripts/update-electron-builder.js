793150f6e59583fdb841fd52e306235b090c7a49
Dawid Urbas <dawid@getbold.agency>
2024-06-18 13:18:00 +0200

    Add script to set name

---
+++ scripts/update-electron-builder.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const path = require('path');
+const yaml = require('js-yaml');
+
+// Paths to the files
+const packageJsonPath = path.join(__dirname, '..', 'package.json');
+const electronBuilderYmlPath = path.join(__dirname, '..', 'electron-builder.yml');
+
+// Read package.json
+const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'));
+
+// Extract the holochain version
+const holochainVersion = packageJson.binaries.holochain;
+
+// Read electron-builder.yml
+const electronBuilderYml = yaml.load(fs.readFileSync(electronBuilderYmlPath, 'utf8'));
+
+// Update the productName
+electronBuilderYml.productName = `Holochain Launcher (${holochainVersion})`;
+
+// Write the updated electron-builder.yml back to the file
+fs.writeFileSync(electronBuilderYmlPath, yaml.dump(electronBuilderYml), 'utf8');
+
+console.log(
+  `Updated productName to "Holochain Launcher (${holochainVersion})" in electron-builder.yml`,
+);
