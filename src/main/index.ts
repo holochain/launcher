bae6d94fa875e7555578f253b8c843c4114ca8db
Matthias <tuttiacc@hotmail.com>
2024-07-25 15:00:02 +0200

    rename to shorten path, quit when all windows closed

--- src/main/index.ts
+++ src/main/index.ts
@@ -313,6 +313,7 @@ app.whenReady().then(async () => {
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
+  app.quit();
   // if (process.platform !== 'darwin') {
   //   app.quit();
   // }
