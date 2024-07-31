e284983b3b70e84689212cad20aee71ee2201e04
Dawid Urbas <dawid@getbold.agency>
2024-07-31 16:18:25 +0200

    Fix

--- scripts/check-todos.sh
+++ scripts/check-todos.sh
@@ -1,15 +1,18 @@
+#!/bin/bash
+
 BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
-SPECIFIC_BRANCH="feat/enhancements-2 "
+SPECIFIC_BRANCH="feat/enhancements-2"
 
-echo BRANCH_NAME: $BRANCH_NAME
-echo SPECIFIC_BRANCH: $SPECIFIC_BRANCH
+echo "BRANCH_NAME: $BRANCH_NAME"
+echo "SPECIFIC_BRANCH: $SPECIFIC_BRANCH"
 
-if [ "$BRANCH_NAME" = "$SPECIFIC_BRANCH" ]; then
-  # Search for TODOs in the code
-  if grep -r "TODO" .; then
-    echo "Warning: There are TODOs in the code. Please check them before pushing."
-    exit 1
-  fi
+if [ "$BRANCH_NAME" != "$SPECIFIC_BRANCH" ]; then
+  exit 0
+fi
+
+if grep -r "TODO(release-check)" --include=\*.{html,ts,svelte} . --exclude-dir={.git,node_modules}; then
+  echo "Warning: There are TODOs in HTML or TypeScript files. Use --no-verify if you still want to proceed."
+  exit 1
 fi
 
 exit 0
